c7837903aca09c8d555bd4e971d994cabb82bb25
brent@blacklabelagency.com <brent@blacklabelagency.com>
2024-04-29 13:16:10 -0400

    active class for work tabs

--- app/components/work/WorkTabs.tsx
+++ app/components/work/WorkTabs.tsx
@@ -1,12 +1,22 @@
 'use client'
+
+import { useEffect, useState } from "react";
+
 export default function WorkTabs({workList, workTabClick}:{workList:Array<string>, workTabClick:(workType:string)=>any}){
+
+    const [currentWorkType, setCurrentWorkType] = useState("recent");
+
+    const handleWorkTabClick = (workType:string) => {
+        setCurrentWorkType(workType);
+        workTabClick(workType);
+    }
 
     return (
         <div className="work-tabs w-full m-auto">
             <div className="flex items-center justify-center mt-6">
                 {workList.map((worktype:string, i:number) => {
                     return (
-                        <button className="px-4" key={`worktab-`+i} onClick={()=>workTabClick(worktype)}>{worktype.toUpperCase()}</button>    
+                        <button className={`px-4 ${worktype==currentWorkType ? "active" : ""}`} key={`worktab-`+i} onClick={()=>handleWorkTabClick(worktype)}>{worktype.toUpperCase()}</button>    
                     )
                 })}
             </div>
